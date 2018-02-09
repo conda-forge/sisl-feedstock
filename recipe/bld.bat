e5443b5078697cf9f5eeb23b6dd63dda6da8d6b8
Nick Papior <nickpapior@gmail.com>
2018-02-09 08:25:07 +0100

    trying to fix compiler issue on win
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,4 @@
+"%PYTHON%" setup.py build_ext --compiler=mingw32 --record record_build.txt
+if errorlevel 1 exit 1
 "%PYTHON%" setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
