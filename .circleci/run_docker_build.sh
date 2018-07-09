702ca4277d8d9bbe98c9156ca24d13d83e01e370
Nick Papior <nickpapior@gmail.com>
2018-07-09 14:43:01 +0200

    checks
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- .circleci/run_docker_build.sh
+++ .circleci/run_docker_build.sh
@@ -58,6 +58,10 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
+
+conda build --check /recipe_root
+env
+cat /recipe_root/meta.yaml
 conda build /recipe_root || exit 1
 upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
 
