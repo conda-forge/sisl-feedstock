0c7cc1931eacb5f5837dea1dd5b55080ac8d74d9
Nick Papior <nickpapior@gmail.com>
2019-04-03 15:35:42 +0200

    added ldflags
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,11 @@
 export OPENBLAS_NUM_THREADS=1
 export LIBRARY_PATH="${PREFIX}/lib"
 export LD_LIBRARY_PATH="${PREFIX}/lib"
+# Add shared
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+    export LDFLAGS="$LDFLAGS -shared"
+fi
 
 $PYTHON setup.py build
 $PYTHON setup.py install
