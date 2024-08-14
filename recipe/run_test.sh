4ca55558932190c1e7d04aff1cbfc15c3a7ad063
Nick Papior <nickpapior@gmail.com>
2024-08-14 10:46:09 +0200

    added debug-info print-out
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Echo out build-information
+echo "<<< Printing out debug-information for the build-process"
+$PYTHON -c 'from sisl._debug_info import * ; print_debug_info()'
 
 # Test command:
+echo "<<< Running tests"
 pytest --pyargs sisl
